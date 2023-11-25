29c8868bcefb8082f85dd82a3681661147610e1a
danik1341 <danik1341@gmail.com>
2023-11-25 15:15:31 +0200

    app home page routed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import ThemeSwitch from "./components/theme-switch";
+import ThemeContextProvider from "./context/theme-context";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="!scroll-smooth h-full">
+      <div className="relative h-full bg-opacity-100 bg-gray-50/10 text-gray-950 pt-28 sm:pt-36 dark:bg-gray-900/10 dark:text-gray-50 dark:text-opacity-90">
+        {/* <div className="flex min-h-screen flex-col m-auto min-w-[300px] max-w-7xl p-4"> */}
+        <div className=" bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" />
+        <div className=" bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
+        <ThemeContextProvider>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+
+          <Toaster position="top-right" />
+          <ThemeSwitch />
+        </ThemeContextProvider>
+      </div>
     </div>
   );
 }
